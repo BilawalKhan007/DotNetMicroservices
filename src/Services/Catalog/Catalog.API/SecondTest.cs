cf9eed519061eed4da2cc128480cd42c6809c47e
Pakistan1947-stack <bilawalkhanx59@gmail.com>
2021-06-09 01:22:58 +0500

    new think

---
+++ src/Services/Catalog/Catalog.API/SecondTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API
+{
+    public class SecondTest
+    {
+        public int One { get; set; }
+    }
+}
